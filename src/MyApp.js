59a85819e788d430514b28b8386e9757a9d9d511
areed08 <areed21@calpoly.edu>
2021-01-15 13:35:13 -0800

    First commit of my react app

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,27 +1,78 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter (index) {
+  useEffect(() => {
+    fetchAll().then( result => {
+        if (result)
+            setCharacters(result);
+     });
+  }, [] );
+
+  async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5000/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+ }
+ 
+  /*function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
        return i !== index
     });
     setCharacters(updated);
+  }*/
+  async function removeOneCharacter (index){
+   try {
+      var updated = characters.filter((character, i) => {
+         return i !== index
+      });
+      setCharacters(updated);
+      var id = characters[index].id
+      const response = await axios.delete('http://localhost:5000/users/' + id);
+      return response.data.users_list;     
+   }
+   catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+   }
+ }
+
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result)
+       setCharacters([...characters, person] );
+    });
+ }
+
+  async function fetchAll(){
+    try {
+       const response = await axios.get('http://localhost:5000/users');
+       return response.data.users_list;     
+    }
+    catch (error){
+       //We're not handling errors. Just logging into the console.
+       console.log(error); 
+       return false;         
+    }
   }
   
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
-
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
   )
+
 }
 
 export default MyApp;
